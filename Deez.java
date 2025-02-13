ca2dbf6ab0a62fd808bb30633fcefe47f121fd12
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2025-02-13 17:23:54 +0000

    jteazjtdzjyrgfdxjytbfuyngfutkyhfd7iryntd75oltuydlrfyzo8el6yidtsulusfdtedgxhfdcufgycvxdehtgcniyktgfhmuilygjvh,luygvhtl8ypugjhutlf,hcbyfkltugfhcv kxkiytgdkmhyfjutghjfylgygulijj haoigaiougouahOSHLCU HDA Uhoduhcoiuhdsiuoihvp;sfioh vpusgoi yvgwsaihdgvhwsahgvdhvsdhavOIF OS GI VYHIS GUIGIUg dg ufs ou gvuhuir fhgdoiu  isrogiyfvgsoidu giys vhy fidgvioysgdiu ngioygfoyighoigv iug vio gfvhxl dyhxugp vco8uys[dfy ov;iuyhsfd;ouhviugf xiulv hvlxiuzfxh jcvh udifz hv uzdhfxliuvhdliu v sfl vguilzsg cvukcgijgvzildg vgiuzsglgvoo d shhehhaj do ho

--- Deez.java
+++ Deez.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Deez {
 
     public static void main(String []args) {
@@ -7,7 +9,7 @@ public class Deez {
             sum += i;
         } System.out.println("sum: " + sum);
 
-        double egg = 0;
+        double egg = 0.0d;
         for (int c : hhhhaaaa) {//find avg
             egg += c;
         }
@@ -32,5 +34,78 @@ public class Deez {
         for (int nuts : deez) {
             deez[nuts]++;
         }
+        int gggggg = 0;
+        for (int rice : deez) {
+            
+           
+            if (gggggg < rice) {
+                gggggg = rice; //find max
+            }
+        }
+        int kjjhg = 0;
+        for (int i = 0;i < deez.length;i++) {
+            if (deez[i] == gggggg) {
+                kjjhg = i;
+            }
+        }
+        System.out.println("mode: " + kjjhg);
+
+
+        //==============================================arraylist timmme
+
+        ArrayList<Integer> shrek = new ArrayList<Integer>();
+        for (int i = 0; i < ((int) (Math.random() * 6969)); i++) {
+            shrek.add( (int) (Math.random() * 70));
+        }
+        System.out.println(shrek);
+        System.out.println(shrek.size());
+
+        //find sum
+        int mon = 0;
+        for (int my : shrek) {
+            mon += my;
+        }
+        System.out.println(mon);
+        
+        //find avg
+        float tamago = 0.0f;
+        for (int p : shrek) {
+            tamago += p;
+        }
+        tamago /= shrek.size();
+        System.out.println(tamago);
+
+        //find za min ando max
+        int minmin = shrek.get(0);
+        int largemac = shrek.get(0);
+        for (int o : shrek) {
+            if (minmin > o) {
+                minmin = o;
+            }
+            if (largemac < o) {
+                largemac = o;
+            }
+        }
+        System.out.println(minmin);
+        System.out.println(largemac);
+
+//find median'
+        ArrayList<Integer> donkey = new ArrayList<Integer>();
+        donkey.addAll(shrek);
+        int c = 1;
+        if (donkey.size() % 2 == 0) {
+            c++;
+        }
+        
+        donkey.sort(null);
+        for (;donkey.size() > c;) {
+            donkey.remove(0);
+            donkey.remove(donkey.size()-1);
+        }
+        donkey.add(donkey.get(0));
+        if (c == 2) {
+            donkey.set(0, (int)( (donkey.get(1) + donkey.get(0))/2));
+        }
+        System.out.println(donkey.get(0));
     }
 }
